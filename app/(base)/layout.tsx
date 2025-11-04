f23b8f36943e861afbb1942f8865e9ef58141f74
GeorgesTatchum <153494397+GeorgesTatchum@users.noreply.github.com>
2025-11-04 22:45:15 +0100

    :sparkles: start of minimum advertisement display

--- app/(base)/layout.tsx
+++ app/(base)/layout.tsx
@@ -9,12 +9,16 @@ import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import HomeContent from "./home/page";
 import { AnnouncementContent } from "./announcement/page";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { InputsAnnounceSearch } from "../types/InputsAnnounceSearch";
+import { OutlinedInput } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 const navItems = [
         { id: "home", label: "Accueil", icon: HomeIcon, href: "/" },
         { id: "messages", label: "Messages", icon: MailOutlineIcon, href: "/messages" },
         { id: "publish", label: "Publier", icon: AddCircleIcon, href: "/publish" },
-        { id: "annonces", label: "Créer une Annonces", icon: TextSnippetIcon, href: "/annonces" },
+        { id: "annonces", label: "Annonces", icon: TextSnippetIcon, href: "/annonces" },
         { id: "profil", label: "Profil", icon: PermIdentityIcon, href: "/profil" }
         ];
 
@@ -33,8 +37,14 @@ export default function BaseLayout({ children }: Readonly<{ children: React.Reac
     const [currentPage, setcurrentPage] = useState<PageKey>("home");
     const currentNav = navItems.find(item => item.id === currentPage);
     const CurrentContent = contentComponents[currentPage] || HomeContent;
-    
-    let title = "Accueil"
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm<InputsAnnounceSearch>();
+      const onSubmit: SubmitHandler<InputsAnnounceSearch> = (data) => console.log(data);
+
     useEffect(() => {
         window.history.replaceState(null, "/home", "");
     }, []);
@@ -45,7 +55,20 @@ export default function BaseLayout({ children }: Readonly<{ children: React.Reac
                 {
                     currentPage === "annonces" && 
                     <>
-                        
+                        <form action="">
+                            <OutlinedInput 
+                                fullWidth
+                                type="text" 
+                                {...register("keyword", {required: true})} 
+                                error={!!errors.keyword}
+                                sx={{ mb: 2 }} 
+                                placeholder="Rechercher une annonce"
+                                endAdornment={
+                                <button type="submit" style={{ border: 'none', background: 'none', cursor: 'pointer', padding: 0, margin: 0 }}>
+                                    <SearchIcon color="action" />
+                                </button>}
+                                />
+                        </form>
                     </>
                 }
             </div>
